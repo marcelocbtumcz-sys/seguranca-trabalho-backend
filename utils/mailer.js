79aa6ebef6b2a90b225865114cce686a217daddf
marcelocbtumcz-sys <marcelo.cbtu.mcz@gmail.com>
2025-10-07 01:39:02 -0300

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,28 +1,23 @@
-require("dotenv").config();
+// utils/mailer.js
 const nodemailer = require("nodemailer");
 
-async function enviarEmail({ to, subject, html }) {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: `"Suporte SESMT" <${process.env.MAIL_USER}>`,
-      to,
-      subject,
-      html,
-    };
-
-    const info = await transporter.sendMail(mailOptions);
-    console.log("✅ E-mail enviado com sucesso:", info.response);
-  } catch (err) {
-    console.error("❌ Erro ao enviar e-mail:", err);
+// Transporter configurado (Gmail, Outlook, etc.)
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "marcelo.cbtu.mcz@gmail.com", // seu e-mail
+    pass: "jrqz qolz hdwp ynaj"         // senha de app do Gmail
   }
+});
+
+// Função genérica para enviar e-mails
+async function enviarEmail({ to, subject, html }) {
+  return transporter.sendMail({
+    from: '"Suporte - Sistema de Controle SESMT" <marcelo.cbtu.mcz@gmail.com>',
+    to,
+    subject,
+    html
+  });
 }
 
 module.exports = enviarEmail;
